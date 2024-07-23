8496f153f1beb27d15da11fbfed5b5a11988c90e
AngelT-lancervs <aatomala@espol.edu.ec>
2024-07-23 17:42:35 -0500

    query search (requires higher membership)

--- shoscan.py
+++ shoscan.py
@@ -3,7 +3,11 @@ import shodan
 import csv
 import argparse
 import time
+import os
 
+# Fixed output file names
+OUTPUT_FILE = "results.csv"
+SHORT_OUTPUT_FILE = "shortResults"
 
 def write_results(results, output_file, short_output_file):
     """
@@ -53,7 +57,6 @@ def parse_shodan_search(ip_list):
     results = {}
     short_results = {}
 
-    # For all IPs in the result
     for ip in ip_list:
         print("For IP: " + str(ip))
         results[ip] = {}
@@ -63,12 +66,10 @@ def parse_shodan_search(ip_list):
         rest = []
 
         try:
-            # For all data per IP
             for i in ip_list[ip]['data']:
                 tmp_pro = ""
                 tmp_port = ""
                 for j in i:
-                    # Get the protocol
                     if str(j) == "_shodan":
                         tmp_pro = "(" + str(i[j]['module']) + ")"
                         protocol.append(str(i[j]['module']))
@@ -78,7 +79,6 @@ def parse_shodan_search(ip_list):
                     rest.append({str(j): str(i[j])})
                 print(tmp_port + tmp_pro)
 
-            # Store results for short version (ip, port, protocol) or full listing of data in Shodan
             short_results[ip] = {'port': port, 'protocol': protocol}
             results[ip] = {'all': rest}
 
@@ -91,11 +91,57 @@ def parse_shodan_search(ip_list):
     return {'short': short_results, 'full': results}
 
 
+def read_queries(file):
+    """
+    Read a .txt file containing queries separated by newlines.
+
+    :param file: Name of the .txt file
+    :return: List of queries
+    """
+    with open(file, 'r') as f:
+        queries = f.read().splitlines()
+    return queries
+
+def search_shodan_query(api, queries):
+    """
+    Search Shodan with a query.
+
+    :param api: Shodan API instance
+    :param queries: List of query strings
+    :return: Search results
+    """
+    results = []
+    for query in queries:
+        try:
+            results_search = api.search(query)
+            results.append({query: results_search['matches']})
+        except shodan.APIError as e:
+            print('Error: {}'.format(e))
+            results.append({query: []})
+
+    return results
+
+def write_results_to_csv(results):
+    """
+    Write Shodan search results to a CSV file.
+
+    :param results: List of dictionaries with search results for each query
+    """
+    with open(OUTPUT_FILE, mode='w', newline='') as file:
+        csv_writer = csv.writer(file)
+        # Write the header row
+        csv_writer.writerow(['Query', 'Data'])
+
+        # Write the results for each query
+        for result in results:
+            for query, matches in result.items():
+                for match in matches:
+                    match_data = [query] + [str(value) for value in match.values()]
+                    csv_writer.writerow(match_data)
+
 if __name__ == '__main__':
-    SHODAN_API_KEY = "7lfMFJPGhPkkXbLv54eXO7Z2VHJSaJIG"
+    SHODAN_API_KEY = os.getenv('SHODAN_API_KEY')
     api = shodan.Shodan(SHODAN_API_KEY)
-    output_file = "results.csv"
-    short_output_file = "shortResults"
 
     if SHODAN_API_KEY == "YOURKEY":
         print("Please edit the script and replace 'SHODAN_API_KEY' with your real key value")
@@ -103,54 +149,39 @@ if __name__ == '__main__':
 
     # Argument parser
     parser = argparse.ArgumentParser(description='Port scanning through Shodan.io')
-    parser.add_argument('--filename', '-f', default='iplist.txt', required=True)
-    parser.add_argument('--fileout', '-o', default=output_file)
+    parser.add_argument('--queryfile', '-q', help='File containing Shodan queries', required=True)
+    parser.add_argument('--filename', '-f', help='File containing IPs (optional if queryfile is provided)')
     args = parser.parse_args()
 
-    # Read IPs from file
-    with open(args.filename, 'r') as f:
-        ips = [line.strip() for line in f]
+    if args.filename:
+        # Read IPs from file
+        with open(args.filename, 'r') as f:
+            ips = [line.strip() for line in f]
 
-    # Output info
-    art = """
-   ,-,--.  ,--.-,,-,--,   _,.---._      ,-,--.    _,.----.    ,---.      .-._         
- ,-.'-  _\/==/  /|=|  | ,-.' , -  `.  ,-.'-  _\ .' .' -   \ .--.'  \    /==/ \  .-._  
-/==/_ ,_.'|==|_ ||=|, |/==/_,  ,  - \/==/_ ,_.'/==/  ,  ,-' \==\-/\ \   |==|, \/ /, / 
-\==\  \   |==| ,|/=| _|==|   .=.     \==\  \   |==|-   |  . /==/-|_\ |  |==|-  \|  |  
- \==\ -\  |==|- `-' _ |==|_ : ;=:  - |\==\ -\  |==|_   `-' \\==\,   - \ |==| ,  | -|  
- _\==\ ,\ |==|  _     |==| , '='     |_\==\ ,\ |==|   _  , |/==/ -   ,| |==| -   _ |  
-/==/\/ _ ||==|   .-. ,\\==\ -    ,_ //==/\/ _ |\==\.       /==/-  /\ - \|==|  /\ , |  
-\==\ - , //==/, //=/  | '.='. -   .' \==\ - , / `-.`.___.-'\==\ _.\=\.-'/==/, | |- |  
- `--`---' `--`-' `-`--`   `--`--''    `--`---'              `--`        `--`./  `--` 
-                                                              by PathetiQ - 2019/03 
-    """
-    print(art)
-    print("------------------------------------------")
-    print("Using input file: " + args.filename)
-    print("Output file - all Shodan's details: " + args.fileout)
-    print("Output file - short version (ip,port,protocol): " + args.fileout + "." + short_output_file)
-    print("------------------------------------------\n")
-    print("Launching search...")
-    print("Results will be displayed after search is completed...")
+        # Search Shodan for each IP
+        ip_info = {}
+        for ip in ips:
+            time.sleep(1)  # Forced Shodan threshold
+            try:
+                hostinfo = api.host(ip)
+                ip_info[ip] = hostinfo
+            except shodan.APIError as e:
+                ip_info[ip] = '{}'.format(e)
 
-    # Search Shodan for each IP
-    ip_info = {}
-    for ip in ips:
-        time.sleep(1)  # Forced Shodan threshold
-        try:
-            hostinfo = api.host(ip)
-            ip_info[ip] = hostinfo
-        except shodan.APIError as e:
-            ip_info[ip] = '{}'.format(e)
+        # Format the data
+        d = json.dumps(ip_info)
+        ip_list = json.loads(d)
 
-    # Format the data
-    d = json.dumps(ip_info)
-    ip_list = json.loads(d)
+        # Parse the data
+        res = parse_shodan_search(ip_list)
+        write_results(res, OUTPUT_FILE, SHORT_OUTPUT_FILE)
+    
+    else:
+        # Read queries from file
+        queries = read_queries(args.queryfile)
 
-    # Parse the data
-    res = parse_shodan_search(ip_list)
-
-    # Write results to CSV
-    write_results(res, args.fileout, short_output_file)
+        # Perform search and write results
+        results = search_shodan_query(api, queries)
+        write_results_to_csv(results)
 
     print("Results have been written to the output files.")
